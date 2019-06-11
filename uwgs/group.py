b77b3773f9205a10dd4ac31a586463bfc278a5dd
Cliff Spital <cspital@uw.edu>
2019-06-10 17:05:04 -0700

    begin adding endpoint specific model types from documentation

--- uwgs/group.py
+++ uwgs/group.py
@@ -1,5 +1,6 @@
 import requests
 from .types import Payload, get_headers
+import attr
 
 
 class Group:
@@ -10,3 +11,18 @@ class Group:
     def get(self, group_id: str, header: dict) -> Payload:
         self._url += '/group/{}'.format(group_id)
         return Payload(self._session.get(self._url, headers=header))
+
+@attr.s
+class GroupResponseMeta:
+    resource_type: str = attr.ib(default='group')
+    version: str = attr.ib(default='')
+    reg_id: str = attr.ib(default='')
+    id: str = attr.ib(default='')
+    self_ref = attr.ib(default='')
+    member_ref: str = attr.ib(default='')
+    timestamp: int = attr.ib(default=0)
+
+@attr.s
+class Group:
+    reg_id: str = attr.ib(default='')
+    
