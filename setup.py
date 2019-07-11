bdc5bbdb7cd6ea17fb8974471fb2a04f8c6ef3bc
Cliff Spital <cspital@uw.edu>
2019-07-11 10:16:24 -0700

    manual pypi upload prep

--- setup.py
+++ setup.py
@@ -8,7 +8,13 @@ setup(
     version='0.2.0',
     description='Flexible http client for reading UW Groups API v3.',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='UW Medicine Research IT',
     url='https://github.com/uwrit/uwgs',
-    packages=['uwgs']
+    packages=['uwgs'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
 )
